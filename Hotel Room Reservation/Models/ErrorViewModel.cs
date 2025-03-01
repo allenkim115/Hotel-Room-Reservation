88f73b30fb806a013aba2df21e39daf495680ba3
Allen <154535686+allenkim115@users.noreply.github.com>
2025-03-01 14:37:34 +0800

    Delete Hotel Room Reservation directory

--- Hotel Room Reservation/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Hotel_Room_Reservation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
