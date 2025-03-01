86f9fed3f825dfc7a7b60c077e87fe3de078ca94
unknown <XX933@ccs.local>
2025-03-01 14:31:23 +0800

    Initial commit

---
+++ Hotel Room Reservation and Management System/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Hotel_Room_Reservation_and_Management_System.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel_Room_Reservation_and_Management_System.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Landing()
+            {
+                return View();
+            }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Hotel Room Reservation and Management System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hotel_Room_Reservation_and_Management_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
